5eb4e32e66cf91ca0003dbb9c9920fb939cc6fbb
Pernilla Lumikero <113108559+pernillalumikero@users.noreply.github.com>
2023-04-11 21:25:22 +0200

    blobs with border-radius

--- client-template/src/pages/Products.jsx
+++ client-template/src/pages/Products.jsx
@@ -4,15 +4,16 @@ const Products = () => {
   return (
     <div>Products
       <div className="wrapper">
-      <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+      {/* <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
         <path fill="#ccd5ae" d="M51.5,-18.8C59.4,7.3,53.2,36,35.8,48.4C18.4,60.8,-10.1,57,-28.1,42.9C-46.2,28.8,-53.8,4.4,-47.3,-19.8C-40.9,-44.1,-20.5,-68.2,0.7,-68.4C21.9,-68.6,43.7,-45,51.5,-18.8Z" transform="translate(100 100)" />
       </svg>
       </div>
       <div className="wrapper">
       <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
         <path fill="#e9edc9" d="M41.8,-65.4C55.7,-56.1,69.7,-47.1,71.9,-35C74.1,-22.8,64.7,-7.4,57.9,5.1C51.1,17.7,47,27.4,41.1,37.4C35.2,47.3,27.5,57.4,16.2,65.4C4.9,73.3,-10.1,79.2,-24.2,77.4C-38.3,75.5,-51.7,65.9,-57.6,53.2C-63.5,40.4,-62,24.6,-59.7,11.3C-57.4,-2,-54.4,-12.7,-52.7,-27.3C-51.1,-41.9,-50.9,-60.4,-42.3,-72.1C-33.8,-83.9,-16.9,-89,-1.5,-86.7C13.9,-84.4,27.9,-74.7,41.8,-65.4Z" transform="translate(100 100)" />
-      </svg>
+      </svg> */}
       </div>
+      <img className='pictures' src="https://images.unsplash.com/photo-1533616688419-b7a585564566?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80" alt="bouquet" />
     </div>
     
   )
