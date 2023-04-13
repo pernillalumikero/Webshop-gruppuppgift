f2e5c22f925276b8383de192512f59afa40342e3
Pernilla Lumikero <113108559+pernillalumikero@users.noreply.github.com>
2023-04-13 14:42:59 +0200

    buttons styled + arrow animated

--- client-template/src/pages/Products.jsx
+++ client-template/src/pages/Products.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Product from '../components/Product';
+import { motion } from 'framer-motion'
 
 const Products = () => {
 
@@ -20,13 +21,36 @@ const Products = () => {
     console.log(error)
   }
 }
+const ballStyle = {
+  display: "inline-block",
+  width: "1rem",
+  height: "1rem",
+  borderRadius: "0.5rem"
+};
+
+const bounceTransition = {
+  y: {
+    duration: 0.8,
+    repeat: Infinity,
+    repeatType: "mirror",
+    ease: "easeOut"
+  }
+};
+
 
   return (
     <div>
       <Hero>
-        <div></div>
-        <h2>Experiance the power of flowers</h2>
-        <button>Shop now</button>
+        <div id="dimmer-div"></div>
+          <h2>Experiance the <span>power</span> of <span>flowers</span></h2>
+          <button>Shop now <motion.span
+        style={ballStyle}
+        transition={bounceTransition}
+        animate={{
+          y: ["40%", "-40%"],
+        }}>&darr;
+      </motion.span>
+      </button>
       </Hero>
       <ProductList>
         {products.map(product => 
@@ -37,24 +61,34 @@ const Products = () => {
   )
 }
 
+
 const Hero = styled.div `
   background-image: url(../src/assets/heropicture.jpg);
   height: 100vh;
   width: 100vw;
   background-size: cover;
-  div {
+
+  #dimmer-div {
     height: 100vh;
     width: 100vw;
     background-color: black;
     opacity: 0.4;
   }
+
   h2 {
-    color: white;
-    font-size: 3rem;
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
+    color: white;
+    font-size: 3.4rem;
+    span:first-child {
+      font-family: italiana;
+    }
+    span:last-child {
+      font-family: italianno;
+      font-size: 4.5rem;
+    }
   }
   button {
     all: unset;
