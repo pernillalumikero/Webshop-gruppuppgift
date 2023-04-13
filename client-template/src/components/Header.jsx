0eaf280402095a44cbcf3b72b7027b07bc96a7b9
Pernilla Lumikero <113108559+pernillalumikero@users.noreply.github.com>
2023-04-13 10:45:02 +0200

    nav intialized

--- client-template/src/components/Header.jsx
+++ client-template/src/components/Header.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Nav from './Nav'
+import styled from 'styled-components'
 
 const Header = () => {
   return (
-    <nav>
-      <Nav />
-    </nav>
+    <HeaderStyling>
+      <nav>
+        <Nav />
+      </nav>
+    </HeaderStyling>
   )
 }
+
+const HeaderStyling = styled.header `
+background-color: #D5BDAF;
+`
 
 export default Header
--- client-template/src/components/Nav.jsx
+++ client-template/src/components/Nav.jsx
@@ -1,15 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
 const Nav = () => {
   return (
-    <nav>
+    <Navigation>
       <Link to="/">Home</Link>
-      {/* <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link> */}
+      <Link to="#">About</Link>
+      <Link to="#">Contact</Link>
+      <Link to="/admin"><img id="user" src="../src/assets/user.png" alt="Admin log in"/></Link>
       <Link to="/admin">Admin</Link>
-    </nav>
+      <Link to="#"><img src="../src/assets/shopping.png" alt="Shopping bag"/></Link>
+
+    </Navigation>
   )
 }
+
+const Navigation = styled.nav`
+  padding: 20px;
+
+  a {
+    padding: 30px;
+    color: black;
+    text-transform: uppercase;
+    font-family: jost;
+    text-decoration: none;
+      img {
+        height: 20px;
+      }
+      #user {
+        height: 25px;
+        position: absolute;
+      }
+  }
+`
 
 export default Nav
